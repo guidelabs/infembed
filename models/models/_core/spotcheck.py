9f27e3c1e7b5eed50572014252a9fe061e75f665
Fulton Wang <fulton@guidelabs.ai>
2024-01-25 18:35:43 +0000

    add spotcheck

---
+++ models/models/_core/spotcheck.py
@@ -0,0 +1,13 @@
+from torchvision.models import resnet18
+import torch
+
+
+def get_spotcheck_model(checkpoint_path, device):
+    import pdb
+    pdb.set_trace()
+    model = resnet18()
+    model.fc = torch.nn.Linear(in_features=512, out_features=1)
+    model.load_state_dict(torch.load(open(checkpoint_path, "rb"), map_location=device))
+    model.to(device=device)
+    model.eval()
+    return model
