54f96cb9f2c8faaf7006ec1ba90e9a5d044fbfab
Fulton Wang <fulton@guidelabs.ai>
2024-02-07 20:23:04 +0000

    standalone imagenet kfac case study

---
+++ examples/imagenet/helpers.py
@@ -0,0 +1,18 @@
+from torchvision.models import ResNet18_Weights
+from torch.utils.data import default_collate
+
+normalize = ResNet18_Weights.IMAGENET1K_V1.transforms()
+
+class ImagenetCollateFn:
+    def __init__(self, device):
+        self.device = device
+
+    def __call__(self, examples):
+        return tuple(
+            [
+                _x.to(device=self.device)
+                for _x in default_collate(
+                    [(normalize(__x[0]), __x[1]) for __x in examples]
+                )
+            ]
+        )
